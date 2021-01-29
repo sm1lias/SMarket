f8b96703b202a0cea62d268b915ff9c2e4864cbb
sm1lias <smiliaressis@gmail.com>
2021-01-29 21:38:39 +0200

    nothing

--- app/src/main/java/com/smilias/smarket/MainActivity.java
+++ app/src/main/java/com/smilias/smarket/MainActivity.java
@@ -2,6 +2,7 @@ package com.smilias.smarket;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,33 +16,49 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.menuHome:
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.menuCart:
-                        Toast.makeText(MainActivity.this, "Cart", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.menuLogin:
-                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        break;
 
-                }
-                return true;
-            }
-        });
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+        // as soon as the application opens the first
+        // fragment should be shown to the user
+        // in this case it is algorithm fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // By using switch we can easily get
+            // the selected fragment
+            // by using there id.
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.menuHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.menuCart:
+                    selectedFragment = new CartFragment();
+                    break;
+                case R.id.menuLogin:
+                    selectedFragment = new LoginFragment();
+                    break;
+            }
+            // It will help to replace the
+            // one fragment to other.
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, selectedFragment)
+                    .commit();
+            return true;
+        }
+    };
+
+
     /*public void login(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
