d00cd710320fb1fbaa3b38f26a7ebd3048a010e2
Giorgos Gewrgios <giorgos974o@hotmail.com>
2021-01-29 20:33:14 +0200

    1

--- app/src/main/java/com/smilias/smarket/MainActivity.java
+++ app/src/main/java/com/smilias/smarket/MainActivity.java
@@ -1,43 +1,15 @@
 package com.smilias.smarket;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
-
-    MyRecyclerViewAdapter adapter;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> animalNames = new ArrayList<>();
-        animalNames.add("LogIn");
-        animalNames.add("test1");
-        animalNames.add("test2");
-        animalNames.add("test3");
-        animalNames.add("test4");
-
-        // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.recycleViewMenu);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapter(this, animalNames);
-        adapter.setClickListener((MyRecyclerViewAdapter.ItemClickListener) this);
-        recyclerView.setAdapter(adapter);
     }
-
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
-    }
-
-
 }
