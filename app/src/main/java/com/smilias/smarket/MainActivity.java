03f2cfc89c2c4eaead91449f666b171b98ba1593
sm1lias <smiliaressis@gmail.com>
2021-01-29 21:21:17 +0200

    nothing

--- app/src/main/java/com/smilias/smarket/MainActivity.java
+++ app/src/main/java/com/smilias/smarket/MainActivity.java
@@ -1,15 +1,50 @@
 package com.smilias.smarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menuHome:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menuCart:
+                        Toast.makeText(MainActivity.this, "Cart", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menuLogin:
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        break;
+
+                }
+                return true;
+            }
+        });
 
     }
+    /*public void login(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+    }*/
 }
