36c1eeb3b014f70b7debbac52d62b80be285a088
sm1lias <smiliaressis@gmail.com>
2021-02-17 21:29:06 +0200

    nth

--- app/src/main/java/com/smilias/smarket/ItemsFragment.java
+++ app/src/main/java/com/smilias/smarket/ItemsFragment.java
@@ -2,18 +2,37 @@ package com.smilias.smarket;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ItemsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ItemsFragment extends Fragment {
+public class ItemsFragment extends Fragment implements MyRecyclerViewAdapter.ItemClickListener {
+
+    DatabaseReference myRef;
+    RecyclerView recyclerView;
+    FirebaseDatabase database;
+    ArrayList<String> categories= new ArrayList<>();
+    LinearLayoutManager layoutManager;
+    MyRecyclerViewAdapter adapter;
+    String nitem;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,6 +49,7 @@ public class ItemsFragment extends Fragment {
 
     public ItemsFragment(String passedString) {
         // Required empty public constructor
+        nitem=passedString;
     }
 
     /**
@@ -57,12 +77,49 @@ public class ItemsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        myRef.addValueEventListener (new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot MainSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot snapshot : MainSnapshot.child("CATEGORIES").child(nitem).getChildren()){
+
+//                    categories.add(snapshot.getValue(String.class).toString());
+                    categories.add(snapshot.getKey());
+                }
+                adapter = new MyRecyclerViewAdapter(getActivity(), categories);
+                adapter.setClickListener(ItemsFragment.this::onItemClick);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_items, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_items, container, false);
+
+        recyclerView = (RecyclerView)rootView.findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return rootView;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+
     }
 }
