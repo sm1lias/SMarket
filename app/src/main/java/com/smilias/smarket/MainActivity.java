6849e3999e3fe2302ed01caedbdd093eeebf1e1b
Giorgos Gewrgios <giorgos974o@hotmail.com>
2021-02-25 12:36:24 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/smilias/smarket/CheckOutActivity.java

--- app/src/main/java/com/smilias/smarket/MainActivity.java
+++ app/src/main/java/com/smilias/smarket/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
@@ -39,7 +40,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     SharedPreferences preferences;
     public String language,item1,item2,supermarket, notItem;
     public double price;
--- app/src/main/java/com/smilias/smarket/MapsActivity.java
+++ app/src/main/java/com/smilias/smarket/MapsActivity.java
@@ -1,19 +1,28 @@
 package com.smilias.smarket;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    Double x,y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        permission();
     }
 
     /**
@@ -39,8 +50,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng SKLAVENITIS = new LatLng(37.9282742, 23.7613769);
+        LatLng LIDL = new LatLng(37.9270036, 23.7170961);
+        LatLng METRO = new LatLng(37.9297351, 23.7370946);
+        LatLng MY_MARKET = new LatLng(37.9298601, 23.7286832);
+        LatLng AB_VASILOPOYLOS = new LatLng(37.9391139, 23.6699681);
+        mMap.addMarker(new MarkerOptions().position(SKLAVENITIS).title("SUPERMARKET SKLAVENITIS"));
+        mMap.addMarker(new MarkerOptions().position(LIDL).title("SUPERMARKET LIDL"));
+        mMap.addMarker(new MarkerOptions().position(METRO).title("SUPERMARKET METRO"));
+        mMap.addMarker(new MarkerOptions().position(MY_MARKET).title("SUPERMARKET MY_MARKET"));
+        mMap.addMarker(new MarkerOptions().position(AB_VASILOPOYLOS).title("SUPERMARKET AB_VASILOPOYLOS"));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        x = location.getLatitude();
+        y = location.getLongitude();
+        LatLng youAreHere=new LatLng(x,y);
+        mMap.addMarker(new MarkerOptions().position(youAreHere).title("youAreHere"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(youAreHere));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+    public void permission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.
+                    requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},234);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                this);
+        //locationManager.removeUpdates(this);
     }
 }
