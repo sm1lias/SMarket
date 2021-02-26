8bf52c088389aa4b82712f121d5c5a5afc0799ca
sm1lias <smiliaressis@gmail.com>
2021-02-26 20:28:48 +0200

    logout button

--- app/src/main/java/com/smilias/smarket/AdminActivity.java
+++ app/src/main/java/com/smilias/smarket/AdminActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -57,5 +58,13 @@ public class AdminActivity extends AppCompatActivity {
             return true;
         }
     };
+
+
+    public void bLogOut(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
     }
+
+}
 
